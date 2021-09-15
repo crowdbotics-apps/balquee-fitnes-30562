34ee239467950f4d0112570a97f73efb18291817
Crowdbotics <team@crowdbotics.com>
2021-09-15 04:13:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "balquee_fitnes_30562Identifier",
+  url: "https://balquee_fitnes_30562Identifier.botics.co",
+  api: "https://balquee_fitnes_30562Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
